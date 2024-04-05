f2eb8793d8d7ffcba730df13da5bae0f4e9cc2b7
classiuz <francorlloveras@gmail.com>
2024-04-04 21:06:53 -0300

    Order project structure and add home page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,21 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      keyframes: {
+        gradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' }
+        }
+      },
+      animation: {
+        gradient: 'gradient 6s ease infinite alternate'
+      },
+      transitionProperty: {
+        filter: 'filter'
+      }
+    }
   },
   plugins: []
 }
