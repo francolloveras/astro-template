c7f29f49aefa5b66c8a149da001c886159c42943
Franco Lloveras <77513994+francolloveras@users.noreply.github.com>
2024-07-09 14:36:04 -0300

    Add base path in config to work with host

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+  base: 'astro-template', // Delete this Prop to use the Template without basePath.
   integrations: [tailwind()]
 })
